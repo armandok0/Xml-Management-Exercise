a2d425fb835de6767cf67a95b90c957c97c07440
Armando Kostas <armandok163@gmail.com>
2024-09-14 19:49:34 +0300

    Initial Commit

---
+++ src/main/java/gr/codehub/xmlmanagementexercise/XmlManagementExercise.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gr.codehub.xmlmanagementexercise;
+
+/**
+ *
+ * @author armandokostas
+ */
+public class XmlManagementExercise {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
